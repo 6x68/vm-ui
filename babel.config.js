5af57f2666bafd21dabe22d793690a4a0cee1b04
6x68 <249649284+fun-fact-is-that-everything-after-the-plus-sign-is-useless@users.noreply.github.com>
2026-01-26 21:20:00 -0600

    fix: FINALLY it builds

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 export default {
-	extends: `${import.meta.resolve("@gera2ld/plaid/config/babelrc-base")}.js`,
+	extends: `${import.meta.resolve("@gera2ld/plaid/config/babelrc-base").slice(7)}.js`,
 	presets: ["@babel/preset-typescript"],
 	plugins: [
 		[
